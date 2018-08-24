aa628a0f9aec4ede39b604295d6ef2be8cb2f118
Wilson <20050803@leeds.gov.uk>
2018-08-24 16:20:51 +0100

    first push

---
+++ TestCordovaQrCodeReader/TestCordovaQrCodeReader/www/scripts/qrTest.js
@@ -0,0 +1,34 @@
+﻿function scanMyQrCode() {
+
+    
+
+
+
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('#btnScan').addEventListener('click', clickHandler);
+    main();
+});
+
+function clickHandler(element) {
+    // On click Code
+
+    cordova.plugins.barcodeScanner.scan(
+        function (result) {
+            if (!result.cancelled) {
+                $("#resultFormat").text("Barcode type is: " + result.format)
+                $("#resultText").text("Decoded text is: " + result.text);
+            }
+        },
+        function (error) {
+            alert("Scanning failed: " + error);
+        }
+    );
+
+    $("#btnScan").text("RE-SCAN ANOTHER QR CODE");
+}
+
+function main() {
+    // Initialization work goes here.
+}
